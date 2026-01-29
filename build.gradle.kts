932c35fa2750a26ae0b181d18537d36cd6193cff
tzimom <75259597+tzimom@users.noreply.github.com>
2026-01-29 13:28:27 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java-library")
+}
+
+group = "net.tzimom"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+
+    maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.21.11-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
---
+++ src/main/java/net/tzimom/enderchest/EnderChestPlugin.java
@@ -0,0 +1,9 @@
+package net.tzimom.enderchest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EnderChestPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+}
